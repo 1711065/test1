4ab14f2ca26796ecfe069fc8f61609fa83883723
1711065 <104405427+1711065@users.noreply.github.com>
2022-06-02 10:10:35 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,39 @@
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    game.addScore(1)
+music.setVolume(255)
+basic.forever(function () {
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . # # .
-        . # . . .
-        # . # . .
-        # . . # .
-        . . . . #
-        `)
+basic.forever(function () {
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) > 5) {
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.playTone(880, music.beat(BeatFraction.Whole))
+        music.playTone(784, music.beat(BeatFraction.Whole))
+        music.playTone(880, music.beat(BeatFraction.Whole))
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Whole))
+        music.playTone(880, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(880, music.beat(BeatFraction.Whole))
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.playTone(880, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(784, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Double))
+    }
 })
